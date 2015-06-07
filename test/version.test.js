b50c2eb976a4776c7e1f9e5a89cec9caeede4f55
Kyle Mitchell <kyle@kemitchell.com>
2015-06-07 07:44:03 +0000

    Test with tape and covert

--- test/version.test.js
+++ test/version.test.js
@@ -1,15 +1,7 @@
-/* jshint mocha: true */
 var version = require('..');
-var expect = require('chai').expect;
 
-describe('version', function() {
-  it('accepts a valid version', function() {
-    expect(version('1.0.0'))
-      .to.equal(true);
-  });
-
-  it('rejects v- prefixed version strings', function() {
-    expect(version('v1.0.0'))
-      .to.equal(false);
-  });
+require('tape')('version', function(test) {
+  test.equal(version('1.0.0'), true, 'accepts a valid version');
+  test.equal(version('v1.0.0'), false, 'rejects v-prefixed versions');
+  test.end();
 });
